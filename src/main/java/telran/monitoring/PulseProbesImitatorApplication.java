19fcefc297f7a36fe52d5cc118d9fa3908811525
stanislav-revva <nivadrive1@gmail.com>
2023-10-22 20:16:18 +0300

    working code

---
+++ src/main/java/telran/monitoring/PulseProbesImitatorApplication.java
@@ -0,0 +1,37 @@
+package telran.monitoring;
+
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.monitoring.dto.PulseProbe;
+import telran.monitoring.service.PulseProbeImitator;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class PulseProbesImitatorApplication {
+private static final long TIMEOUT = 30000;
+final PulseProbeImitator imitator;
+	
+	public static void main(String[] args) throws InterruptedException {
+		ConfigurableApplicationContext ctx =  SpringApplication.run(PulseProbesImitatorApplication.class, args);
+		Thread.sleep(TIMEOUT);
+		ctx.close();
+	}
+	@Bean
+	Supplier<PulseProbe> probesSupplier() {
+		return () -> {
+			PulseProbe probe = imitator.nextProbe();
+			log.trace("{}",probe);
+			return probe;
+		};
+		
+	}
+
+}
